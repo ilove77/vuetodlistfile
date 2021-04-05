259ae848ee306a03461f4dcdfc8784c7a8a5f2be
yizhi <ian8703031919@gmail.com>
2021-04-05 15:39:51 +0800

    finishss

--- backcode/model/database.js
+++ backcode/model/database.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 // 连接数据库
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true })
+if(process.env.NODE_ENV == "production"){
+    mongoose.connect(
+        "mongodb+srv://yizhi:h94u454s06g4@vuetodolist.woh4b.mongodb.net/vuetodolist?retryWrites=true&w=majority",{useNewUrlParser: true})
+    .then(() => {
+        console.log("成功")
+    })
+    .catch(() => {
+        console.log("失敗")
+    })
+}
+else{
+mongoose.connect('mongodb://localhost:27017/login', {useNewUrlParser: true })
 	.then(() => console.log('數據庫連結成功'))
     .catch(() => console.log('數據庫連結失敗'))
-    
+}
